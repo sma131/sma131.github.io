40a618a6f929c4650a2379f4bb4219655da7ce82
sma131 <sma131@gmail.com>
2016-12-24 12:37:56 +0100

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -446,13 +446,13 @@ var resizePizzas = function(size) {
 
     return dx;
   }
-
-  // Iterates through pizza elements on the page and changes their widths
+ // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var randomPizzas =document.querySelectorAll(".randomPizzaContainer");
+    for(var i=0; i<randomPizzas.length; i++){
+      var dx =determineDx(randomPizzas[i],size);
+      var newwidth = (randomPizzas[i].offsetWidth+dx) + 'px';
+      randomPizzas[i].style.width= newwidth ;
     }
   }
 
